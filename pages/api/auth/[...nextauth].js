bd1423ab7f4915e43eb6afc7cf6ff6f61250831b
omgismartinez <martnzomg@gmail.com>
2022-07-05 22:35:53 -0500

    feat (auth): github provider

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth'
+import GitHub from 'next-auth/providers/github'
+
+export default NextAuth({
+    providers: [
+        GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ]
+})
