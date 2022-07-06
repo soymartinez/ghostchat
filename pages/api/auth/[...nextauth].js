d7c0393e2954221dfa78e4249d2af3c749988255
omgismartinez <martnzomg@gmail.com>
2022-07-06 11:10:48 -0500

    refactor (auth): secret, pages

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,17 @@
 import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
 
-export default NextAuth({
+export const authOptions = {
     providers: [
         GitHub({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
-    ]
-})
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        signIn: '/signin',
+    },
+}
+
+export default NextAuth(authOptions)
