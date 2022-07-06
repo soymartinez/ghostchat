0e284e9dd8ac4f4b1901d2f360e206a33eea6a60
omgismartinez <martnzomg@gmail.com>
2022-07-06 11:13:29 -0500

    feat (auth): signin

---
+++ pages/signin.js
@@ -0,0 +1,42 @@
+import { getProviders, signIn } from 'next-auth/react'
+import Layout from 'components/layout'
+import Github from 'components/github'
+
+export default function Signin({ providers }) {
+    console.log(providers)
+    return (
+        <Layout page={'Login'}>
+            <div className='flex justify-center items-center w-full h-5/6'>
+                <div className='flex flex-auto flex-col'>
+                    <div className='text-center mb-10'>
+                        <h1 className='text-white text-[32px] font-extrabold'>
+                            Log in to Secret Chat
+                        </h1>
+                    </div>
+                    <div className='w-full max-w-xs self-center'>
+                        <div className='pb-4 border-b border-white'>
+                            <button onClick={() => signIn(providers.github.id)}
+                                className='flex justify-center items-center 
+                                          bg-white hover:bg-zinc-100 text-black text-center font-bold
+                                            py-3 min-w-full max-w-full mb-3
+                                            border border-transparent rounded-md transition-all'>
+                                <Github />
+                                <span>Continue whit {providers.github.name}</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const providers = await getProviders(context)
+
+    return {
+        props: {
+            providers,
+        }
+    }
+}
