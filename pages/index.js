5c95c95cb5e121f6dd2670e01822b48fbe55d11a
omgismartinez <martnzomg@gmail.com>
2022-07-06 11:14:09 -0500

    refactor (index): protected page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
+import { useSession } from 'next-auth/react'
 import Form from 'components/form'
 import Icon from 'components/icon'
 import Layout from 'components/layout'
 import { createOrJoinConversation } from 'services/chat'
+import Signin from './signin'
 
 export default function Home({ data }) {
 
+  const { data: session, status } = useSession()
+  const loading = status === 'loading'
+
+  if (typeof window !== "undefined" && loading) return null
+
+  if (!session) {
+    return (
+      <Signin />
+    )
+  }
 
   return (
     <Layout page={`Secret chat`}>
@@ -13,7 +25,7 @@ export default function Home({ data }) {
             w-full h-5/6'>
         <div className='flex flex-col justify-center items-center'>
           <Icon />
-          <h1 className='font-bold text-5xl text-white mb-4'>Secret chat</h1>
+          <h1 className='font-bold text-5xl text-white mb-4 tracking-tight'>Secret chat</h1>
           <Form />
         </div>
       </div>
