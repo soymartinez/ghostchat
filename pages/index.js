33a71572abfc4aa3a707db28b0925e9ef216e260
omgismartinez <martnzomg@gmail.com>
2022-07-06 22:54:21 -0500

    refactor (index): 🔄 session

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,16 @@ import { authOptions } from './api/auth/[...nextAuth]'
 import { useEffect, useState } from 'react'
 
 export default function Home({ data }) {
-  const [session, setSession] = useState({})
-  const { name, email, image } = session.user
+  const [session, setSession] = useState(null)
 
   useEffect(() => {
     async function session() {
-      const user = await getSession()
-      setSession({
-        user: user.user,
-      })
+      const { user } = await getSession()
+      setSession(user)
     }
     session()
-  }, []);
-
+  }, [])
+  
   return (
     <Layout page={`Secret chat`}>
       <div className='
@@ -30,10 +27,10 @@ export default function Home({ data }) {
           <Icon />
           <h1 className='font-bold text-5xl text-white mb-4 tracking-tight'>Secret chat</h1>
           {
-            session.user && (
+            session && (
               <div className='flex justify-between items-center 
-                              w-full transition-all mb-4' key={email}>
-                <span className='text-md text-white font-semibold'>{name}</span>
+                              w-full transition-all mb-4' key={session.email}>
+                <span className='text-md text-white font-semibold'>{session.name}</span>
                 <button className='text-md text-black font-bold 
                                   bg-white hover:bg-black hover:text-white
                                   px-3 py-1 rounded-full transition-all' onClick={() => signOut()}>
