ae5861dc9fc2d22dd7bd49e0d7317f9d37e5b4ca
omgismartinez <martnzomg@gmail.com>
2022-07-06 21:56:56 -0500

    refactor (index): get user from await getSession

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
-import { useSession } from 'next-auth/react'
+import { unstable_getServerSession } from 'next-auth/next'
+import { getSession, signOut } from 'next-auth/react'
 import Form from 'components/form'
 import Icon from 'components/icon'
 import Layout from 'components/layout'
 import { createOrJoinConversation } from 'services/chat'
-import Signin from './signin'
+import { authOptions } from './api/auth/[...nextAuth]'
+import { useEffect, useState } from 'react'
 
 export default function Home({ data }) {
+  const [session, setSession] = useState({})
+  const { name, email, image } = session.user
 
-  const { data: session, status } = useSession()
-  const loading = status === 'loading'
-
-  if (typeof window !== "undefined" && loading) return null
-
-  if (!session) {
-    return (
-      <Signin />
-    )
-  }
+  useEffect(() => {
+    async function session() {
+      const user = await getSession()
+      setSession({
+        user: user.user,
+      })
+    }
+    session()
+  }, []);
 
   return (
     <Layout page={`Secret chat`}>
@@ -26,6 +29,19 @@ export default function Home({ data }) {
         <div className='flex flex-col justify-center items-center'>
           <Icon />
           <h1 className='font-bold text-5xl text-white mb-4 tracking-tight'>Secret chat</h1>
+          {
+            session.user && (
+              <div className='flex justify-between items-center 
+                              w-full transition-all mb-4' key={email}>
+                <span className='text-md text-white font-semibold'>{name}</span>
+                <button className='text-md text-black font-bold 
+                                  bg-white hover:bg-black hover:text-white
+                                  px-3 py-1 rounded-full' onClick={() => signOut()}>
+                  Sign Out
+                </button>
+              </div>
+            )
+          }
           <Form />
         </div>
       </div>
@@ -39,6 +55,8 @@ export async function getServerSideProps() {
   createOrJoinConversation('test', data.token)
 
   return {
-    props: { data },
+    props: {
+      data,
+    },
   }
 } 
