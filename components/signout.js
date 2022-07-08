618f76b6e873865cb3baf9b2fcbc57c657f7df49
omgismartinez <martnzomg@gmail.com>
2022-07-07 19:53:54 -0500

    refactor (signout): hover active from props

--- components/signout.js
+++ components/signout.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Signout() {
+export default function Signout({ active }) {
     return (
-        <svg 
+        <svg
             width="20px"
             height="20px"
             viewBox="0 0 24 24"
@@ -11,14 +11,14 @@ export default function Signout() {
         >
             <path
                 d="M13 12H22M22 12L18.6667 8M22 12L18.6667 16"
-                stroke="black"
+                stroke={`${active ? 'white' : 'black'}`}
                 strokeWidth={2}
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M14 7V5.1736C14 4.00352 12.9999 3.08334 11.8339 3.18051L3.83391 3.84717C2.79732 3.93356 2 4.80009 2 5.84027V18.1597C2 19.1999 2.79733 20.0664 3.83391 20.1528L11.8339 20.8195C12.9999 20.9167 14 19.9965 14 18.8264V17"
-                stroke="black"
+                stroke={`${active ? 'white' : 'black'}`}
                 strokeWidth={2}
                 strokeLinecap="round"
                 strokeLinejoin="round"
