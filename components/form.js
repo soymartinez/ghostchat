418460483e6e776056cb836d435c8f20b7be11a0
omgismartinez <martnzomg@gmail.com>
2022-07-07 23:57:05 -0500

    refactor (form): added logic behind submit button and ux improvement

--- components/form.js
+++ components/form.js
@@ -2,18 +2,33 @@ import { useState } from 'react'
 
 export default function Form() {
     const [name, setName] = useState('')
-    function handleAnonymousSubmit(e) {
+    function handleCreateOrJoinRoomSubmit(e) {
         e.preventDefault()
-        console.log(name)
+        if (name.length === 0) { return setName('secret') }
+        if (name !== 'secret') {
+            // TODO: join room
+            console.log("join room: " + name)
+        }
     }
 
     return (
-        <form onSubmit={handleAnonymousSubmit}>
-            <input type='text' placeholder='Anonimous name'
-                className='border-2 rounded-full p-2 outline-none transition-all
+        <form onSubmit={handleCreateOrJoinRoomSubmit}
+            className='relative flex justify-center items-center w-full'>
+            <input type='text' placeholder='room name'
+                className={`border-2 rounded-full p-2 outline-none transition-all
                             font-semibold text-center focus:text-black
-                            focus:outline-none focus:border-[#888888]'
+                            focus:outline-none focus:border-zinc-300 w-full
+                            ${name === 'secret'
+                        ? 'focus:border-red-500 border-red-500'
+                        : 'border-zinc-300'}`}
                 onChange={(e) => setName(e.target.value)} />
+            <span className={`
+                absolute bottom-0 transition-all
+                ${name === 'secret'
+                    ? 'z-20 -bottom-8 text-red-500'
+                    : '-z-20'}`}>
+                room name is <strong>required</strong>
+            </span>
         </form>
     )
 }
