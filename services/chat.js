103a93d0978ba7d628791279b20fc990360ee636
omgismartinez <martnzomg@gmail.com>
2022-07-08 20:20:52 -0500

    refactor (chat): 🔄 return a promise

--- services/chat.js
+++ services/chat.js
@@ -3,16 +3,21 @@ import { Client } from '@twilio/conversations'
 export function createOrJoinConversation(room, token) {
     const client = new Client(token)
 
-    client.on('stateChanged', async (state) => {
-        if (state === 'initialized') {
-            let conversation
-            try {
-                conversation = await client.createConversation({
-                    uniqueName: room,
-                })
-            } catch (error) {
-                conversation = await client.getConversationByUniqueName(room)
+    return new Promise(resolve => {
+        client.on('stateChanged', async (state) => {
+            if (state === 'initialized') {
+                let conversation
+
+                try {
+                    conversation = await client.createConversation({ uniqueName: room })
+                    // conversation.join()
+                } catch (error) {
+                    conversation = await client.getConversationByUniqueName(room)
+                    console.log('error: ', error)
+                }
+                console.log('conversation: ', conversation)
+                resolve(conversation)
             }
-        }
+        })
     })
 }
