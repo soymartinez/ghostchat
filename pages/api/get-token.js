1b351e676ab19faa7895896758eae2f7c2a93e80
omgismartinez <martnzomg@gmail.com>
2022-07-08 23:59:38 -0500

    feat (api): get token

--- pages/api/get-token.js
+++ pages/api/get-token.js
@@ -1,14 +1,37 @@
-import { tokenGenerator } from 'services/token-service'
+import twilio from 'twilio'
+
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
+const TWILIO_API_KEY = process.env.TWILIO_API_KEY
+const TWILIO_API_SECRET = process.env.TWILIO_API_SECRET
+const SERVICE_SID = process.env.SERVICE_SID
 
 export default async function getToken(req, res) {
     switch (req.method) {
         case 'GET':
-            const identity = req.query.identity
-            const token = tokenGenerator(identity)
+            // const identity = req.headers.identity
+
+            // if (identity == null) {
+            //     res.json({ status: 401 })
+            // }
+
+            const AccessToken = twilio.jwt.AccessToken
+            const ChatGrant = AccessToken.ChatGrant
+
+            const token = new AccessToken(
+                TWILIO_ACCOUNT_SID,
+                TWILIO_API_KEY,
+                TWILIO_API_SECRET
+            )
+
+            const chatGrant = new ChatGrant({
+                serviceSid: SERVICE_SID
+            })
+
+            token.addGrant(chatGrant)
 
             res.json({
-                identity,
-                token: token.token.toJwt(),
+                status: 200,
+                token: token.toJwt(),
             })
     }
 }
