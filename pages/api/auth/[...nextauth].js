67c1da76947e3ac88d80a881d9803d6e16dd2439
omgismartinez <martnzomg@gmail.com>
2022-07-09 16:59:04 -0500

    refactor (nextauth): 🔄 added callback (jtw, session) property

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,40 @@
 import NextAuth from 'next-auth'
+import { getToken } from 'next-auth/jwt'
 import GitHub from 'next-auth/providers/github'
 
 export const authOptions = {
     providers: [
         GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
+            clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
     pages: {
         signIn: '/signin',
     },
+    callbacks: {
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token
+                // const tokenNextAuth = await getToken({ req, secret })
+                // token.accessToken = tokenNextAuth.
+            }
+            return token
+        },
+        async session({ session, token, user }) {
+            // Send properties to the client, like an access_token from a provider.
+            session.accessToken = token.accessToken
+            if (session.user.email === 'martnzomg@gmail.com') {
+                session.user.role = 'admin'
+            } else {
+                session.user.role = 'user'
+            }
+            return session
+        }
+    },
+
 }
 
 export default NextAuth(authOptions)
