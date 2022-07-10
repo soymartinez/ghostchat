ead4cbdaad733d101ea9259b008b8e2f47252996
omgismartinez <martnzomg@gmail.com>
2022-07-09 23:00:08 -0500

    refactor (index): 🔄 user was fetched via getToken and the account information was separated into the header component

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,17 @@
 import { unstable_getServerSession } from 'next-auth/next'
-import { getSession, signOut } from 'next-auth/react'
-import Form from 'components/form'
-import Icon from 'components/icon'
-import Layout from 'components/layout'
-import { createOrJoinConversation } from 'services/chat'
+import { getToken } from 'next-auth/jwt'
 import { authOptions } from './api/auth/[...nextAuth]'
-import { useEffect, useState } from 'react'
-import Signout from 'components/signout'
-import Image from 'next/image'
 
-export default function Home({ data }) {
-  const [session, setSession] = useState(null)
-  const [hover, setHover] = useState(false);
+import Layout from 'components/layout'
+import Header from 'components/header'
+import Form from 'components/form'
 
-  useEffect(() => {
-    async function session() {
-      const { user } = await getSession()
-      setSession(user)
-    }
-    session()
-  }, [])
-
+export default function Home({ user }) {
   return (
     <Layout page={`Secret chat`}>
-      <div className='
-            flex justify-center items-center
-            w-full h-5/6'>
-        <div className='flex flex-col justify-center items-center'>
-          <Icon />
-          <h1 className='font-bold text-5xl text-white mb-4 tracking-tight'>Secret chat</h1>
-          {
-            session && (
-              <div className='flex justify-between items-center 
-                              w-full transition-all mb-4 gap-8' key={session.email}>
-                <div className='flex items-center gap-2'>
-                  <Image src={session.image} className='rounded-full' width={32} height={32} />
-                  <span className='text-md text-white font-semibold'>{session.name}</span>
-                </div>
-                <button className='text-md text-black font-bold 
-                                  flex justify-center items-center gap-1 px-3 py-1 rounded-full transition-all
-                                  bg-white hover:bg-transparent border-2
-                                  hover:text-white hover:border-white'
-                  onMouseEnter={() => setHover(true)}
-                  onMouseLeave={() => setHover(false)}
-                  onClick={() => signOut()}>
-                  <Signout active={hover} /> Sign Out
-                </button>
-              </div>
-            )
-          }
+      <div className='flex justify-center items-center h-5/6'>
+        <div className='flex flex-col justify-center items-center w-full'>
+          {user && (<Header user={user} />)}
           <Form />
         </div>
       </div>
@@ -72,13 +35,14 @@ export async function getServerSideProps(context) {
     }
   }
 
-  const req = await fetch('http://localhost:3000/api/get-token')
-  const data = await req.json()
-  createOrJoinConversation('test', data.token)
+  const accessTokenNextAuth = await getToken({
+    req: context.req,
+    secret: process.env.NEXTAUTH_SECRET
+  })
 
   return {
     props: {
-      data,
+      user: accessTokenNextAuth,
     },
   }
 } 
