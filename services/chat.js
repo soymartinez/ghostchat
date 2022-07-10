5ca8186aa5b1126397f2a090289206d105346968
omgismartinez <martnzomg@gmail.com>
2022-07-09 22:49:31 -0500

    refactor (service): 🔄 props and conversations events (join or create)

--- services/chat.js
+++ services/chat.js
@@ -1,21 +1,20 @@
 import { Client } from '@twilio/conversations'
 
-export function createOrJoinConversation(room, token) {
+export async function createOrJoinConversation({ room, token }) {
     const client = new Client(token)
-
     return new Promise(resolve => {
-        client.on('stateChanged', async (state) => {
+        client.on('stateChanged', async state => {
             if (state === 'initialized') {
                 let conversation
 
                 try {
-                    conversation = await client.createConversation({ uniqueName: room })
-                    // conversation.join()
-                } catch (error) {
                     conversation = await client.getConversationByUniqueName(room)
+                } catch (error) {
+                    conversation = await client.createConversation({ uniqueName: room })
                     console.log('error: ', error)
                 }
                 console.log('conversation: ', conversation)
+
                 resolve(conversation)
             }
         })
