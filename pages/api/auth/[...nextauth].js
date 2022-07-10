ef1bf237b5bc9c7f3de8e07ccb4104a98fab553d
omgismartinez <martnzomg@gmail.com>
2022-07-10 17:14:10 -0500

    refactor (nextauth): 🔄 remove censers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth'
-import { getToken } from 'next-auth/jwt'
 import GitHub from 'next-auth/providers/github'
 
 export const authOptions = {
@@ -15,16 +14,12 @@ export const authOptions = {
     },
     callbacks: {
         async jwt({ token, account }) {
-            // Persist the OAuth access_token to the token right after signin
             if (account) {
                 token.accessToken = account.access_token
-                // const tokenNextAuth = await getToken({ req, secret })
-                // token.accessToken = tokenNextAuth.
             }
             return token
         },
-        async session({ session, token, user }) {
-            // Send properties to the client, like an access_token from a provider.
+        async session({ session, token }) {
             session.accessToken = token.accessToken
             if (session.user.email === 'martnzomg@gmail.com') {
                 session.user.role = 'admin'
