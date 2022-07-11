6d75b7384fe867e6a8e8974bcb94fa34cb197fe0
omgismartinez <martnzomg@gmail.com>
2022-07-10 22:55:24 -0500

    refactor (conversation): 🔄 renamed state name to message and reset value input after send message

--- components/conversation.js
+++ components/conversation.js
@@ -1,18 +1,16 @@
-import { useRouter } from "next/router"
+import { useConversation } from "context/context"
 import { useState } from "react"
 
 export default function Conversation() {
-    const [name, setName] = useState('')
-    const router = useRouter()
-
-    let message = ''
-    let messages = []
+    const [message, setMessage] = useState('')
+    const { conversationContext } = useConversation()
 
     async function handleSendMessageSubmit(e) {
         e.preventDefault()
-        if (name.length === 0 || name === 'secret') { return setName('secret') }
+        if (message.length === 0 || message === 'secret') { return setMessage('secret') }
 
-        
+        conversationContext.sendMessage(message)
+        setMessage('')
     }
     return (
         <div>
@@ -22,13 +20,14 @@ export default function Conversation() {
                     className={`border-2 rounded-full p-2 outline-none transition-all
                             font-semibold text-center focus:text-black
                             focus:outline-none focus:border-zinc-300 w-full
-                            ${name === 'secret'
+                            ${message === 'secret'
                             ? 'focus:border-red-500 border-red-500'
                             : 'border-zinc-300'}`}
-                    onChange={(e) => setName(e.target.value)} />
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)} />
                 <span className={`
                 absolute bottom-0 transition-all
-                ${name === 'secret'
+                ${message === 'secret'
                         ? 'z-20 -bottom-8 text-red-500'
                         : '-z-20'}`}>
                     message is <strong>required</strong>
