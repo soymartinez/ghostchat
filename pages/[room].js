48b7ff3ecb8262db6976041376dd82edd2f2fb5b
omgismartinez <martnzomg@gmail.com>
2022-07-13 21:32:36 -0500

    refactor (room): 🔄 renamed to refer to users

--- pages/[room].js
+++ pages/[room].js
@@ -1,21 +1,21 @@
 import { unstable_getServerSession } from 'next-auth'
 import { getToken } from 'next-auth/jwt'
 import { useRouter } from 'next/router'
-import { authOptions } from './api/auth/[...nextAuth]'
+import { authOptions } from './api/auth/[...nextauth]' 
 
 import Layout from 'components/layout'
 import ConversationInput from 'components/conversation-input'
 import Conversation from 'components/conversation'
 import Header from 'components/header'
 
-export default function Room({ data }) {
+export default function Room({ user }) {
   const { query } = useRouter()
   
   return (
     <Layout page={`${query.room}`}>
       <div className='flex justify-center items-center h-5/6'>
         <div className='flex flex-col justify-center items-center w-full'>
-          <Header user={data} room={query.room} />
+          <Header user={user} room={query.room} />
           <Conversation room={query.room} />
           <ConversationInput />
         </div>
@@ -38,18 +38,6 @@ export async function getServerSideProps(context) {
         permanent: false,
       }
     }
-  } else {
-    // try {
-    //   joinConversation({room: context.params, token: getAccessToken()})
-
-    // } catch (e) {
-    //   return {
-    //     redirect: {
-    //       destination: '/',
-    //       permanent: false,
-    //     }
-    //   }
-    // }
   }
 
   const accessTokenNextAuth = await getToken({
@@ -59,7 +47,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      data: accessTokenNextAuth,
+      user: accessTokenNextAuth,
     }
   }
 }
