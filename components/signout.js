6472b3cd2c8925274410e339e8e6f3536e432469
omgismartinez <martnzomg@gmail.com>
2022-07-16 23:53:45 -0500

    delete (signout): ⛔ remove transition and edit strokeWigth, colors

--- components/signout.js
+++ components/signout.js
@@ -8,18 +8,16 @@ export default function Signout({ active }) {
             xmlns="http://www.w3.org/2000/svg"
         >
             <path
-            className='transition-all'
                 d="M13 12H22M22 12L18.6667 8M22 12L18.6667 16"
-                stroke={`${active ? '#6c727a' : '#e5e5e5'}`}
-                strokeWidth={2}
+                stroke={`${active ? '#fff' : '#8f939a'}`}
+                strokeWidth={2.5}
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
-            className='transition-all'
                 d="M14 7V5.1736C14 4.00352 12.9999 3.08334 11.8339 3.18051L3.83391 3.84717C2.79732 3.93356 2 4.80009 2 5.84027V18.1597C2 19.1999 2.79733 20.0664 3.83391 20.1528L11.8339 20.8195C12.9999 20.9167 14 19.9965 14 18.8264V17"
-                stroke={`${active ? '#6c727a' : '#e5e5e5'}`}
-                strokeWidth={2}
+                stroke={`${active ? '#fff' : '#8f939a'}`}
+                strokeWidth={2.5}
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
