3bd69b3251ed8e7d7b782c3108f4aa7451a1f681
omgismartinez <martnzomg@gmail.com>
2022-07-16 23:50:40 -0500

    feat (hash): 🚀 svg icon #

---
+++ components/hash.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Hash() {
+    return (
+        <svg
+            width="18px"
+            height="18px"
+            viewBox="0 0 256 256"
+            fill='#42a0ff'
+            className='rotate-[70deg]'
+            strokeWidth={'10px'}
+            stroke='#42a0ff'
+            id="Flat"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M216,148H172V108h44a12,12,0,0,0,0-24H172V40a12,12,0,0,0-24,0V84H108V40a12,12,0,0,0-24,0V84H40a12,12,0,0,0,0,24H84v40H40a12,12,0,0,0,0,24H84v44a12,12,0,0,0,24,0V172h40v44a12,12,0,0,0,24,0V172h44a12,12,0,0,0,0-24Zm-108,0V108h40v40Z" />
+        </svg>
+    )
+}
