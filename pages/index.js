0783a729e79fef7afc98a51fb0c526c5a19c48be
omgismartinez <martnzomg@gmail.com>
2022-07-19 02:46:13 -0500

    refactor (index): 🔄 remove getToken method and add reset chat from context

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,26 @@
 import { useEffect } from 'react'
 import { unstable_getServerSession } from 'next-auth/next'
-import { getToken } from 'next-auth/jwt'
 import { authOptions } from './api/auth/[...nextauth]'
 
-import { useUser } from 'context/context'
+import { useChat } from 'context/context'
 import Layout from 'components/layout'
 import Header from 'components/header'
 import Form from 'components/form-select-room'
 
-export default function Home({ user }) {
-  const { setUserContext } = useUser()
+export default function Home() {
+  const { resetChat } = useChat()
 
   useEffect(() => {
     document.getElementById('main').style.opacity = 1
-    setUserContext(user)
+    resetChat()
   }, []);
 
   return (
     <Layout page={`Secret chat`}>
       <div className='flex justify-center items-center h-5/6'>
         <div className='flex flex-col justify-center items-center w-full duration-500
-          bg-[#242628] rounded-2xl p-4 opacity-0' id='main'>
-          <Header user={user} />
+          opacity-0 relative' id='main'>
+          <Header />
           <Form />
         </div>
       </div>
@@ -45,14 +44,7 @@ export async function getServerSideProps(context) {
     }
   }
 
-  const accessTokenNextAuth = await getToken({
-    req: context.req,
-    secret: process.env.NEXTAUTH_SECRET
-  })
-
   return {
-    props: {
-      user: accessTokenNextAuth,
-    },
+    props: {},
   }
 } 
