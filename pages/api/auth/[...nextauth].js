f62c5d2fbe8c7de775764786d04bcc3a61ea8c02
omgismartinez <martnzomg@gmail.com>
2022-07-19 23:47:46 -0500

    refactor (nextauth): 🔄 using single instance of primaClient

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,7 @@
 import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from 'lib/prisma'
 
 export const authOptions = {
     adapter: new PrismaAdapter(prisma),
