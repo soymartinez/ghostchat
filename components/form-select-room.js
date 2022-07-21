733e9c6894c94aa5cb2152bd302087488f56f29b
omgismartinez <martnzomg@gmail.com>
2022-07-20 23:57:19 -0500

    refactor (form-select-room): 🔄 input design and syntaxis

--- components/form-select-room.js
+++ components/form-select-room.js
@@ -5,22 +5,19 @@ import { useChat } from 'context/context'
 
 export default function Form() {
     const { push } = useRouter()
-    const [name, setName] = useState('')
+    const [chatname, setChatname] = useState('')
     const [error, setError] = useState(false)
 
     const { createOrJoinChat } = useChat()
 
     async function handleCreateOrJoinRoomSubmit(e) {
         e.preventDefault()
-        if (name.length === 0 || name === 'secret') { return setName('secret') }
+        if (chatname.length === 0 || chatname === 'secret') { return setChatname('secret') }
 
         try {
-            const room = await createOrJoinChat(name)
-            if (room) {
-                push(`/${name}`)
-            } else {
-                setError(true)
-            }
+            const room = await createOrJoinChat(chatname)
+            if (room) push(`/${chatname}`)
+            else setError(true)
         } catch (error) {
             console.log('error', error)
         }
@@ -28,28 +25,34 @@ export default function Form() {
 
     return (
         <form onSubmit={handleCreateOrJoinRoomSubmit}
-            className='relative flex justify-center items-center w-full bg-[#242628] rounded-2xl p-4 '>
-            <input type='text' placeholder='room name' autoFocus
-                className={`rounded-lg p-2 outline-none transition-all bg-transparent
-                            font-semibold text-center focus:text-white border-2 border-[#151617]
-                            focus:outline-none focus:border-zinc-300 w-full
-                            ${name === 'secret' || error ? 'focus:border-red-500 border-red-500' : ''}`}
-                onChange={(e) => {
-                    setName(e.target.value)
-                    setError(false)
-                }} />
-            <span className={`${error || name === 'secret' ? 'z-20 -bottom-6' : '-z-20 bottom-0'}
-                        absolute rotate-45 rounded-sm transition-all
-                        w-3 h-3 bg-[#252728]`}></span>
-            <div className={`
-                absolute bottom-0 transition-all rounded-xl w-auto text-center
-                ${name === 'secret' ? 'z-20 bg-[#252728] -bottom-12 px-4 py-1' : '-z-20 opacity-0'}`}>
-                room name is <strong>required</strong>
-            </div>
-            <div className={`
-                absolute bottom-0 transition-all rounded-xl w-auto text-center
-                ${error ? 'z-20 bg-[#252728] -bottom-12 px-4 py-1' : '-z-20 opacity-0'}`}>
-                You are not authorized to access this room 😢
+            className='w-full relative'>
+            <div className={`flex justify-center items-center 
+                            border-2 ${error || chatname === 'secret' ? 'border-red-500' : 'border-[#262728]'}
+                            rounded-xl py-2 pl-4 pr-2 w-full`}>
+                <input type='text' placeholder='Write a chat name' autoFocus
+                    className={`outline-none transition-all w-full lowercase
+                                bg-transparent placeholder:text-[#3e4044] focus:border-zinc-300
+                            ${chatname === 'secret' || error
+                            ? 'focus:border-red-500 border-red-500 placeholder:text-red-500'
+                            : 'placeholder:text-[#3e4044] border-zinc-300'}`}
+                    onChange={(e) => {
+                        setChatname(e.target.value.toLowerCase())
+                        setError(false)
+                    }} />
+                <span className={`
+                    ${error || chatname === 'secret' ? 'z-20 -bottom-5' : '-z-20 opacity-0'}
+                        absolute bottom-0 rotate-45 rounded-sm transition-all
+                        w-3 h-3 bg-red-500`}></span>
+                <div className={`
+                    absolute bottom-0 transition-all rounded-xl w-auto text-center
+                    ${chatname === 'secret' ? 'z-20 bg-red-500 -bottom-11 px-4 py-1' : '-z-20 opacity-0'}`}>
+                    chat name is <strong>required</strong>
+                </div>
+                <div className={`
+                    absolute bottom-0 transition-all rounded-xl w-auto text-center
+                    ${error ? 'z-20 bg-[#252728] -bottom-12 px-4 py-1' : '-z-20 opacity-0'}`}>
+                    You are not authorized to access this room 😢
+                </div>
             </div>
         </form>
     )
