c756df3b6fafcf7542845c50c9347ee6d005150f
omgismartinez <martnzomg@gmail.com>
2022-07-20 23:55:57 -0500

    refactor (conversation): 🔄 design and fix 🎯 scrollbotton, infinte console.log

--- components/conversation.js
+++ components/conversation.js
@@ -7,10 +7,13 @@ export default function Conversation({ user, room }) {
   const { chat } = useChat()
 
   useEffect(() => {
-    getCurrentMessages()
     scrollToBottom()
     getNewMessages()
-  }, [messages, chat])
+  }, [messages])
+
+  useEffect(() => {
+    getCurrentMessages()
+  }, [chat])
 
   function scrollToBottom() {
     const chatBox = document.getElementById('chatBox')
@@ -19,8 +22,8 @@ export default function Conversation({ user, room }) {
 
   async function getCurrentMessages() {
     try {
-      const getMessages = await chat.getMessages()
-      setMessages(getMessages.items)
+      const messages = await chat.getMessages(200)
+      setMessages(messages.items)
     } catch (error) {
       console.log('#1 .. 🤲 GET CURRENT MESSAGES')
     }
@@ -45,33 +48,35 @@ export default function Conversation({ user, room }) {
   }
 
   return (
-    <div className='w-full rounded-md h-screen overflow-auto' id='chatBox'>
+    <div className='w-full rounded-md h-screen overflow-auto
+        scrollbar-thin scrollbar-thumb-blue-500' id='chatBox'>
       {
-        messages.map(({ author, body, dateCreated, index }) => (
-          <div key={index}
-            className={`${body.length < 3 ? 'w-min' : ''}
+        messages.length > 0 ?
+          messages.map(({ author, body, dateCreated, index }) => (
+            <div key={index}
+              className={`${body.length < 3 ? 'w-min' : ''}
                     flex gap-4
                     first:mt-0 last:mb-0 my-2 p-3
                     bg-[#1a1b1c] rounded-xl`}>
-            <div className='flex'>
-              <Image src={user.username.toLowerCase() === author ? user.image : 'https://avatars.githubusercontent.com/u/72507996?v=4'} alt={author} className='rounded-full'
-                placeholder='blur' blurDataURL='#151617' layout='fixed' width={45} height={45} />
-            </div>
-            <div>
-              <div className='flex items-center gap-2'>
-                <span className={`text-white font-bold self-baseline whitespace-nowrap leading-normal
-                  ${body.length < 3 ? 'hidden' : ''}`}>
-                  {author}
-                </span>
-                <span className={`text-[#666971] text-sm font-semibold whitespace-nowrap leading-normal
-                  ${body.length < 3 ? 'hidden' : ''}`}>
-                  {dateCreated.toLocaleString('en-US', options)}
-                </span>
+              <div className='flex'>
+                <Image src={user.username.toLowerCase() === author ? user.image : 'https://avatars.githubusercontent.com/u/72507996?v=4'} alt={author} className='rounded-full'
+                  placeholder='blur' blurDataURL='#151617' layout='fixed' width={45} height={45} />
               </div>
-              <p className={`leading-tight ${body.length < 3 ? 'text-4xl px-2' : ''}`}>{body}</p>
+              <div>
+                <div className='flex items-center gap-2'>
+                  <span className={`text-white font-bold self-baseline whitespace-nowrap leading-normal
+                  ${body.length < 3 ? 'hidden' : ''}`}>
+                    {author}
+                  </span>
+                  <span className={`text-[#666971] text-sm font-semibold whitespace-nowrap leading-normal
+                  ${body.length < 3 ? 'hidden' : ''}`}>
+                    {dateCreated.toLocaleString('en-US', options)}
+                  </span>
+                </div>
+                <p className={`leading-tight ${body.length < 3 ? 'text-4xl px-2' : ''}`}>{body}</p>
+              </div>
             </div>
-          </div>
-        ))
+          )) : <p className='text-center text-lg font-semibold text-[#42a0ff]'>{messages.length > 1 ? 'No messages yet 👻' : 'loading...' }</p>
       }
     </div>
   )
