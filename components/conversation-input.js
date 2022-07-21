915955298820fcc35a771da9f4f9b8c1f58a234e
omgismartinez <martnzomg@gmail.com>
2022-07-20 23:53:54 -0500

    refactor (conversation-input): 🔄 input element

--- components/conversation-input.js
+++ components/conversation-input.js
@@ -28,23 +28,16 @@ export default function ConversationInput({ user }) {
                             : null
                     }
                 </div>
-                <div className='flex justify-center items-center 
-                                border-2 border-[#262728]
-                                rounded-xl py-2 pl-4 pr-2 w-full'>
+                <div className={`flex justify-center items-center transition-all
+                                border-2 ${message === 'secret' ? 'border-red-500' : 'border-[#262728]'} 
+                                rounded-xl py-2 pl-4 pr-2 w-full`}>
                     <input type='text' placeholder='Write a message' id='message' autoFocus autoComplete="off"
                         className={`outline-none transition-all w-full
-                            bg-transparent placeholder:text-[#3e4044] focus:border-zinc-300
-                            ${message === 'secret'
-                                ? 'focus:border-red-500 border-red-500'
-                                : 'border-zinc-300'}`}
+                            bg-transparent focus:border-zinc-300
+                            ${message === 'secret' 
+                            ? 'placeholder:text-red-500 focus:border-red-500 border-red-500' 
+                            : 'placeholder:text-[#3e4044] border-zinc-300'}`}
                         onChange={(e) => setMessage(e.target.value)} />
-                    <span className={`
-                        absolute bottom-0 transition-all
-                        ${message === 'secret'
-                            ? 'z-20 -bottom-8 text-red-500'
-                            : '-z-20'}`}>
-                        message is <strong>required</strong>
-                    </span>
                     <button
                         type='submit'
                         className='absolute right-2
