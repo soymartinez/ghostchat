14f323016bd562e141c9de854140f30e22112821
omgismartinez <martnzomg@gmail.com>
2022-07-21 10:44:14 -0500

    feat (form-select-room): 🚀 loader

--- components/form-select-room.js
+++ components/form-select-room.js
@@ -2,24 +2,30 @@ import { useState } from 'react'
 import { useRouter } from 'next/router'
 
 import { useChat } from 'context/context'
+import Loader from './loader'
 
 export default function Form() {
     const { push } = useRouter()
     const [chatname, setChatname] = useState('')
     const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
 
     const { createOrJoinChat } = useChat()
 
     async function handleCreateOrJoinRoomSubmit(e) {
         e.preventDefault()
         if (chatname.length === 0 || chatname === 'secret') { return setChatname('secret') }
-
+        
         try {
+            setLoading(true)
             const room = await createOrJoinChat(chatname)
             if (room) push(`/${chatname}`)
             else setError(true)
         } catch (error) {
             console.log('error', error)
+            setLoading(false)
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -50,9 +56,12 @@ export default function Form() {
                 </div>
                 <div className={`
                     absolute bottom-0 transition-all rounded-xl w-auto text-center
-                    ${error ? 'z-20 bg-[#252728] -bottom-12 px-4 py-1' : '-z-20 opacity-0'}`}>
+                    ${error ? 'z-20 bg-red-500 -bottom-11 px-4 py-1' : '-z-20 opacity-0 bg-[#252728]'}`}>
                     You are not authorized to access this room 😢
                 </div>
+                {
+                    loading ? <Loader size={22} /> : null
+                }
             </div>
         </form>
     )
