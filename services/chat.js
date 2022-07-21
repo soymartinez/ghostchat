10095ae87b90a1d1ebe33501df5cc3f12c9acfaa
omgismartinez <martnzomg@gmail.com>
2022-07-21 00:01:19 -0500

    refactor (chat): 🔄 added news functions from twilio client

--- services/chat.js
+++ services/chat.js
@@ -11,13 +11,13 @@ export async function createOrJoinConversation({ uniqueName, friendlyName, token
                     conversation = await client.getConversationByUniqueName(uniqueName)
                 } catch (error) {
                     if (error.message === 'Forbidden') {
-                        console.log('You are not authorized to access this room 😢')
+                        return console.log('You are not authorized to access this room 😢')
                     } else {
                         conversation = await client.createConversation({
                             uniqueName,
                             friendlyName,
                         })
-                        
+
                         await conversation.join()
                     }
                     console.log('error: ', error)
@@ -26,4 +26,39 @@ export async function createOrJoinConversation({ uniqueName, friendlyName, token
             }
         })
     })
+}
+
+export async function getSubscribedChats(token) {
+    const client = new Client(token)
+    const chatList = await client.getSubscribedConversations()
+    return chatList.items
+}
+
+export async function deleteChat(token, chatSid) {
+    const client = new Client(token)
+    const conversation = await client.getConversationBySid(chatSid)
+    conversation.delete()
+}
+
+export async function joinChat(token, uniqueName) {
+    const client = new Client(token)
+    const conversation = await client.getConversationByUniqueName(uniqueName)
+    return conversation
+}
+
+export async function addParticipant(token, uniqueName, username) {
+    const client = new Client(token)
+    const conversation = await client.getConversationByUniqueName(uniqueName)
+    return conversation.add(username)
+}
+
+export async function updateParticipant(token, uniqueName, username, body) {
+    const client = new Client(token)
+    const conversation = await client.getConversationByUniqueName(uniqueName)
+    const participant = await conversation.getParticipantByIdentity(username)
+    const update = await participant.attributes({
+        friendlyName: body.friendlyName,
+        image: body.image,
+    })
+    return update
 }
