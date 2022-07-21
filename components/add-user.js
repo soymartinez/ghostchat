edbd608e2b7a0a72c5a0bf9760c3591d3fc88285
omgismartinez <martnzomg@gmail.com>
2022-07-21 13:09:29 -0500

    refactor (add-user): 🔄 design, loading, addParticipant

--- components/add-user.js
+++ components/add-user.js
@@ -1,73 +1,81 @@
 import { useState } from "react"
-import { useChat } from "context/context"
-import Send from "./send"
+import { addParticipant } from "services/chat"
+import Loader from "./loader"
 
-export default function AddUser() {
+export default function AddUser({ room }) {
   const [username, setUsername] = useState('')
   const [isAdded, setIsAdded] = useState(false)
   const [conflict, setConflict] = useState(false)
-  const { chat } = useChat()
+  const [loading, setLoading] = useState(false)
 
   async function handleJoinUser(e) {
     e.preventDefault()
     if (username.length === 0 || username === 'secret') { return setUsername('secret') }
 
     try {
-      const newParticipant = await chat.add(username)
+      setLoading(true)
+      const newParticipant = await addParticipant(room, username)
       console.log('newParticipant: ', newParticipant)
       document.getElementById('username').value = ''
       setIsAdded(true)
     } catch (error) {
       if (error.message === 'Conflict') {
+        console.log(`It's already in the room 😎`)
         setConflict(true)
-        return console.log(`It's already in the room 😎`)
       }
       console.log('👶 adding a new participant')
+    } finally {
+      setUsername('')
+      setLoading(false)
     }
   }
 
   return (
-    <form onSubmit={handleJoinUser} className={`
+    <div className={`absolute right-0 transition-all rounded-b-3xl 
+                            bg-gradient-to-b from-[#0f0f10] via-[#0f0f10]
+                            w-full h-52 flex justify-center items-center -z-50 top-0
+                            `}>
+      <form onSubmit={handleJoinUser} className={`
       flex justify-center items-center relative transition-all -z-30
       ${conflict || username === 'secret' ? 'border-red-500' : 'border-[#262728]'}
-      border-2 rounded-xl py-2 pl-4 pr-2 w-full bg-[#151617]
+      border-2 rounded-xl py-2 px-4 w-full bg-[#151617]
       ${conflict ? 'border-[#3480cc]' : 'border-[#262728]'}`}>
-      <input type='text' placeholder='Write a username' id='username' autoFocus autoComplete="off"
-        className={`outline-none transition-all w-full lowercase
-          bg-transparent placeholder:text-[#3e4044] focus:border-zinc-300
+        <input type='text' placeholder='Write a username' id='username' autoFocus autoComplete="off"
+          className={`outline-none transition-all w-full lowercase text-center
+          bg-transparent placeholder:text-[#3e4044]
           ${username === 'secret'
-            ? 'focus:border-red-500 border-red-500'
-            : 'border-zinc-300'}`}
-        onChange={(e) => {
-          setUsername(e.target.value.toLowerCase())
-          setConflict(false)
-          setIsAdded(false)
-        }} />
-      <span className={`${conflict || username === 'secret' ? 'z-20 -bottom-6' : '-z-20 bottom-0 opacity-0'}
+              ? 'focus:border-red-500 border-red-500 placeholder:text-red-500'
+              : ''}
+          ${conflict ? 'focus:border-red-500 border-red-500 placeholder:text-red-500' : ''}`}
+          onChange={(e) => {
+            setUsername(e.target.value.toLowerCase())
+            setConflict(false)
+            setIsAdded(false)
+          }} />
+        <span className={`
+        ${conflict || username === 'secret' 
+        ? 'z-20 -bottom-6' : '-z-20 bottom-0 opacity-0'}
         absolute rotate-45 rounded-sm transition-all w-3 h-3
         ${conflict ? 'bg-[#3480cc]' : 'bg-red-500'}`}></span>
-      <div className={`
+        <div className={`
         absolute bottom-0 transition-all rounded-xl w-auto text-center
         ${username === 'secret' ? 'z-20 bg-red-500 -bottom-12 px-4 py-1' : '-z-20 opacity-0'}`}>
-        username is <strong>required</strong>
-      </div>
-      <div className={`
+          username is <strong>required</strong>
+        </div>
+        <div className={`
         absolute bottom-0 transition-all rounded-xl w-auto text-center
         ${conflict ? 'z-20 bg-[#3480cc] -bottom-12 px-4 py-1' : '-z-20 opacity-0'}`}>
-        <strong>{username}</strong> is already in the room 😎
-      </div>
-      <div className={`
+          <strong>{username}</strong> is already in the room 😎
+        </div>
+        <div className={`
         absolute bottom-0 transition-all rounded-xl w-auto text-center
         ${isAdded ? 'z-20 bg-[#42a0ff] -bottom-12 px-4 py-1' : '-z-20 opacity-0'}`}>
-        {username} has joined the room
-      </div>
-      <button
-        type='submit'
-        className='absolute right-2
-        rounded-md p-1 transition-all bg-[#313336] 
-        font-semibold text-center text-white hover:animate-pulse'>
-        <Send />
-      </button>
-    </form>
+          <strong>{username}</strong> has joined the room 😎
+        </div>
+        {
+          loading ? <div className='absolute right-4'> <Loader size={22} /> </div> : null
+        }
+      </form>
+    </div>
   )
 }
