a81dea610f1e1a3826ba07115b64a4ee628bdc1b
omgismartinez <martnzomg@gmail.com>
2022-07-22 17:10:30 -0500

    refactor (add-user): 🔄 design, clear fields when opening input

--- components/add-user.js
+++ components/add-user.js
@@ -1,12 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { addParticipant } from "services/chat"
 import Loader from "./loader"
 
-export default function AddUser({ room }) {
+export default function AddUser({ close, room }) {
   const [username, setUsername] = useState('')
+  const [newUser, setNewUser] = useState(false)
   const [isAdded, setIsAdded] = useState(false)
   const [conflict, setConflict] = useState(false)
   const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const input = document.getElementById('username')
+    if (!close) {
+      setUsername('')
+      input.value = ''
+      setLoading(false)
+      setConflict(false)
+    } else {
+      input.focus()
+    }
+  }, [close])
 
   async function handleJoinUser(e) {
     e.preventDefault()
@@ -15,9 +28,9 @@ export default function AddUser({ room }) {
     try {
       setLoading(true)
       const newParticipant = await addParticipant(room, username)
-      console.log('newParticipant: ', newParticipant)
-      document.getElementById('username').value = ''
+      setNewUser(newParticipant.identity)
       setIsAdded(true)
+      document.getElementById('username').value = ''
     } catch (error) {
       if (error.message === 'Conflict') {
         console.log(`It's already in the room 😎`)
@@ -25,7 +38,6 @@ export default function AddUser({ room }) {
       }
       console.log('👶 adding a new participant')
     } finally {
-      setUsername('')
       setLoading(false)
     }
   }
@@ -33,28 +45,27 @@ export default function AddUser({ room }) {
   return (
     <div className={`absolute right-0 transition-all rounded-b-3xl 
                             bg-gradient-to-b from-[#0f0f10] via-[#0f0f10]
-                            w-full h-52 flex justify-center items-center -z-50 top-0
+                            w-full h-56 flex flex-col justify-center items-start -z-50 top-0
                             `}>
+      <span className='text-sm text-[#666971] pb-1'>
+        You can add a new user even if he is not logged in yet.
+      </span>
       <form onSubmit={handleJoinUser} className={`
       flex justify-center items-center relative transition-all -z-30
-      ${conflict || username === 'secret' ? 'border-red-500' : 'border-[#262728]'}
-      border-2 rounded-xl py-2 px-4 w-full bg-[#151617]
-      ${conflict ? 'border-[#3480cc]' : 'border-[#262728]'}`}>
-        <input type='text' placeholder='Write a username' id='username' autoFocus autoComplete="off"
-          className={`outline-none transition-all w-full lowercase text-center
+      ${username === 'secret' ? 'border-red-500' : conflict ? 'border-[#3480cc]' : 'border-[#262728]'}
+      border-2 rounded-xl py-2 px-4 w-full bg-[#151617]`}>
+        <input type='text' placeholder='Write a github user' id='username' autoFocus autoComplete="off"
+          className={`outline-none transition-all w-full lowercase placeholder:normal-case text-center
           bg-transparent placeholder:text-[#3e4044]
-          ${username === 'secret'
-              ? 'focus:border-red-500 border-red-500 placeholder:text-red-500'
-              : ''}
-          ${conflict ? 'focus:border-red-500 border-red-500 placeholder:text-red-500' : ''}`}
+          ${username === 'secret' ? 'text-red-500 placeholder:text-red-500' : conflict ? 'text-blue-500' : ''}`}
           onChange={(e) => {
             setUsername(e.target.value.toLowerCase())
             setConflict(false)
             setIsAdded(false)
           }} />
         <span className={`
-        ${conflict || username === 'secret' 
-        ? 'z-20 -bottom-6' : '-z-20 bottom-0 opacity-0'}
+        ${conflict || username === 'secret'
+            ? 'z-20 -bottom-6' : '-z-20 bottom-0 opacity-0'}
         absolute rotate-45 rounded-sm transition-all w-3 h-3
         ${conflict ? 'bg-[#3480cc]' : 'bg-red-500'}`}></span>
         <div className={`
@@ -70,7 +81,7 @@ export default function AddUser({ room }) {
         <div className={`
         absolute bottom-0 transition-all rounded-xl w-auto text-center
         ${isAdded ? 'z-20 bg-[#42a0ff] -bottom-12 px-4 py-1' : '-z-20 opacity-0'}`}>
-          <strong>{username}</strong> has joined the room 😎
+          <strong>{newUser}</strong> has joined the room 😎
         </div>
         {
           loading ? <div className='absolute right-4'> <Loader size={22} /> </div> : null
