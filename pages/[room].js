81957103d0ea0a585bdc300291788370c6c92a25
omgismartinez <martnzomg@gmail.com>
2022-07-23 23:58:38 -0500

    refactor (room): 🔄 context now prompts for logic to join a chat.

--- pages/[room].js
+++ pages/[room].js
@@ -1,43 +1,27 @@
 import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import { unstable_getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]'
+import { useChat } from 'context/context'
 
-import { useChat, useUser } from 'context/context'
 import Layout from 'components/layout'
 import ConversationInput from 'components/conversation-input'
 import Conversation from 'components/conversation'
 import Header from 'components/header'
 
 export default function Room({ room }) {
-  const { push } = useRouter()
-
-  const { createOrJoinChat } = useChat()
-  const { user, createUser } = useUser()
+  const { initializeJoinChat } = useChat()
 
   useEffect(() => {
-    isChat()
-    createUser()
+    initializeJoinChat(room)
   }, [])
 
-  async function isChat() {
-    try {
-      const access = await createOrJoinChat(room)
-      if (!access) {
-        push('/')
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
-    <Layout page={`${room}`}>
+    <Layout page={room}>
       <div className='flex justify-center items-center h-screen'>
         <div className='flex flex-col w-full h-full'>
           <Header room={room} />
-          <Conversation user={user} room={room} />
-          <ConversationInput user={user} />
+          <Conversation />
+          <ConversationInput />
         </div>
       </div>
     </Layout>
