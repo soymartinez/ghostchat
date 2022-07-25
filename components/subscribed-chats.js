10d1af39a760f0dfe096de77e3076189c2b56055
omgismartinez <martnzomg@gmail.com>
2022-07-24 23:03:06 -0500

    refactor (subscribed-chats): 🔄 added delete, join button, design, transition

--- components/subscribed-chats.js
+++ components/subscribed-chats.js
@@ -1,8 +1,13 @@
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { deleteChat, getSubscribedChats } from 'services/chat'
-import Loader from './loader'
 import { options } from 'lib/options'
+
+import Loader from './loader'
+import Crown from './crown'
+import Users from './users'
+import Info from './info'
+import Message from './message'
 
 export default function Subscribed() {
     const [chats, setChats] = useState([])
@@ -10,6 +15,10 @@ export default function Subscribed() {
 
     useEffect(() => {
         subscribedChats()
+        if (!loading) {
+            const cardsContent = document.getElementById('cards-content')
+            cardsContent.style.opacity = 1
+        }
     }, [chats])
 
     async function subscribedChats() {
@@ -20,39 +29,74 @@ export default function Subscribed() {
     }
 
     async function deleteSubscribedChat(chatSid) {
+        setLoading(true)
         await deleteChat(chatSid)
             .then(subscribedChats)
             .catch(console.error)
     }
 
     return (
-        <div className='grid grid-flow-row grid-cols-2 gap-2
-            w-full h-80 overflow-auto mt-2 relative
-            scrollbar-thin scrollbar-thumb-blue-500
-            transition-all rounded-2xl'>
+        <div className={`
+            w-full overflow-auto mt-2 relative ${chats.length > 0 ? 'h-full' : 'h-96'}
+            scrollbar-thin scrollbar-thumb-blue-500 
+            transition-all duration-1000 rounded-2xl`} >
             {
                 chats && chats.length > 0 ?
-                    chats.map(({ sid, uniqueName, createdBy, dateCreated, participants }) => (
-                        <div key={sid}>
-                            <Link href={uniqueName || '/'}>
-                                <a>
+                    <div id='cards-content'
+                        className='grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-2 opacity-0'>
+                        {
+                            chats.map(({ sid, uniqueName, createdBy, dateCreated, lastMessage, participants }) => (
+                                <div key={sid}>
                                     <div className='
-                                        flex gap-2
-                                        bg-[#242628] transition-all p-4 rounded-2xl
-                                        cursor-pointer hover:shadow-lg shadow-blue-500 relative'>
-                                        <div className='bg-gradient-to-br from-violet-500 w-10 h-10 rounded-full'></div>
+                                flex flex-col gap-2
+                                bg-[#242628] transition-all p-4 rounded-2xl
+                                cursor-pointer hover:shadow-lg shadow-blue-500 relative'>
+                                        <div className='flex justify-between items-center'>
+                                            <div className='bg-gradient-to-br from-zinc-600 w-16 h-16 rounded-full' />
+                                            <div className='flex flex-col gap-2'>
+                                                <Link href={uniqueName || '/'}>
+                                                    <a>
+                                                        <button className='bg-zinc-700 hover:bg-blue-500 rounded-full px-3 transition-all'>
+                                                            Join
+                                                        </button>
+                                                    </a>
+                                                </Link>
+                                                <button key={sid} onClick={() => deleteSubscribedChat(sid)}
+                                                    className='bg-zinc-700 hover:bg-red-500 rounded-full px-3 transition-all'>
+                                                    {loading ? 'Deleting' : 'Delete'}
+                                                </button>
+                                            </div>
+                                        </div>
                                         <div className=''>
                                             <h1 className='text-md text-blue-500 font-bold'>
                                                 {uniqueName}
                                             </h1>
-                                            <h1 className='text-sm font-bold'>{createdBy}</h1>
-                                            <span className='text-sm'>{dateCreated.toLocaleString('en-US', options)}</span>
+                                            {/* <p className='py-2'>description</p> */}
+                                            <div className='flex flex-col gap-1 overflow-hidden'>
+                                                <div className='flex gap-4'>
+                                                    <h1 className='text-sm font-bold flex gap-2'><Crown /> {createdBy}</h1>
+                                                    <h1 className='text-sm font-bold flex gap-2'>
+                                                        <Users index={true} />
+                                                        {participants.size} <span className='font-medium'> Participants</span>
+                                                    </h1>
+                                                </div>
+                                                <div className='flex gap-4'>
+                                                    <h1 className='text-sm flex gap-2'><Info />
+                                                        {dateCreated.toLocaleString('en-US', options)}
+                                                    </h1>
+                                                    {lastMessage ?
+                                                        <h1 className='text-sm font-bold flex gap-2'>
+                                                            <Message />
+                                                            {lastMessage.index + 1} <span className='font-medium'>Messages</span>
+                                                        </h1> : null}
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </a>
-                            </Link>
-                        </div>
-                    ))
+                                </div>
+                            ))
+                        }
+                    </div>
                     :
                     <div className='absolute grid place-content-center w-full h-full'>
                         {
